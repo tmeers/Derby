b4cb447a9551d71459a118650a2672fb783fdb15
Tim Meers <tim.meers@gmail.com>
2013-12-30 07:45:14 -0500

    Starting to integrate user based functions

--- Derby/Models/Pack.cs
+++ Derby/Models/Pack.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +12,7 @@ namespace Derby.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Region { get; set; }
-        //public Guid CreatedBy { get; set; }
+        public Guid CreatedById { get; set; }
 
         public ICollection<Den> Dens { get; set; }
         public ICollection<Competition> Competitions { get; set; }
