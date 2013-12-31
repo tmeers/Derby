794dc2b7a6693c19b043f2d4789b851b2b4f978a
Tim Meers <tim.meers@gmail.com>
2013-12-31 10:08:51 -0500

    New routes for Pack details

--- Derby/App_Start/RouteConfig.cs
+++ Derby/App_Start/RouteConfig.cs
@@ -37,11 +37,32 @@ namespace Derby
                 url: "packs/",
                 defaults: new { controller = "Pack", action = "Index" }
             );
+            //routes.MapRoute(
+            //    name: "Pack-Edit",
+            //    url: "pack/edit/{id}",
+            //    defaults: new { controller = "Pack", action = "Edit" }
+            //);
+            //routes.MapRoute(
+            //    name: "Pack-Edit",
+            //    url: "pack/delete/{id}",
+            //    defaults: new { controller = "Pack", action = "Delete" }
+            //);
+            routes.MapRoute(
+                name: "Pack-Details",
+                url: "pack/{id}",
+                defaults: new { controller = "Pack", action = "Details" }
+            );
 
             routes.MapRoute(
                 name: "Den-Create",
                 url: "den/create/{packId}",
                 defaults: new { controller = "Den", action = "Create", packId = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Competition-Details",
+                url: "competition/{id}",
+                defaults: new { controller = "Competition", action = "Details" }
             );
 
             routes.MapRouteLowercase(
