af815165257f018bd91bd9494fe843600f3b4bdc
Tim Meers <tim.meers@gmail.com>
2014-01-12 21:59:54 -0500

    New RaceType

---
+++ Derby/Infrastructure/RaceType.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Derby.Infrastructure
+{
+    public enum RaceType
+    {
+        [Display(Name = "Pinewood Derby")]
+        PinewoodDerby,
+        [Display(Name = "Raingutter Regatta")]
+        RaingutterRegatta
+    }
+}
