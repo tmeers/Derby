70473c8c063bb538959d51cc95948f781d586584
Tim Meers <tim.meers@gmail.com>
2014-02-26 14:55:52 -0500

    Prep for running heats

---
+++ Derby/ViewModels/ContestantViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Derby.Models;
+
+namespace Derby.ViewModels
+{
+    public class ContestantViewModel
+    {
+        public int Id { get; set; }
+        [Key]
+        public int RacerId { get; set; }
+        public int ScoutId { get; set; }
+        public int RaceId { get; set; }
+        public string ScoutName { get; set; }
+        public string CarNumber { get; set; }
+        public bool Selected { get; set; }
+       
+        [StringLength(1, ErrorMessage = "The {0} must be at least {2} number long.", MinimumLength = 1)]
+        [RegularExpression("([0-9]+)", ErrorMessage = "Enter only numbers")]
+        public string Lane { get; set; }
+
+
+        public ContestantViewModel() { }
+
+        public ContestantViewModel(Racer racer)
+        {
+            RacerId = racer.Id;
+            ScoutId = racer.ScoutId;
+            CarNumber = racer.CarNumber;
+        }
+    }
+}
---
+++ Derby/ViewModels/RunHeatViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Derby.Models;
+
+namespace Derby.ViewModels
+{
+    public class RunHeatViewModel
+    {
+        public int Id { get; set; }
+        public int RaceId { get; set; }
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Tie Breaker")]
+        public bool TieBreaker { get; set; }
+
+        public int HeatsNeeded { get; set; }
+
+        public int CompetitionId { get; set; }
+        public Competition Competition { get; set; }
+        public ICollection<Heat> CurrentHeats { get; set; }
+        public ICollection<ContestantViewModel> Contestants { get; set; }
+
+        public RunHeatViewModel()
+        {
+            Contestants = new List<ContestantViewModel>();
+        }
+    }
+}
