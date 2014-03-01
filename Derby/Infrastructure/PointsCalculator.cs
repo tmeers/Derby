efbb483b3e00309762477f61ddedd3a38cb23d17
Tim Meers <tim.meers@gmail.com>
2014-02-28 21:58:29 -0500

    Calculating points for each racer

---
+++ Derby/Infrastructure/PointsCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Derby.Infrastructure
+{
+    public class PointsCalculator
+    {
+        public static int Calculate(int laneCount, int place)
+        {
+            // Needs to return the reward value of the place. 
+            // If there are 6 lanes, racer gets 1st place, the net score would be 6.
+            // The last place always gets one point
+            return (laneCount + 1) - place;
+        }
+    }
+}
