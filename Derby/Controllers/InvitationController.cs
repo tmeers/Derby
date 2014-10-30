607af4b9a2de288bb8eab3cc733e13f5d9da635b
Tim Meers <tim.meers@gmail.com>
2014-10-30 11:19:04 -0400

    Updated around CreateInvite

--- Derby/Controllers/InvitationController.cs
+++ Derby/Controllers/InvitationController.cs
@@ -37,12 +37,11 @@ namespace Derby.Controllers
             return View(packinvitation);
         }
 
-        public PartialViewResult CreateInvite()
+        public PartialViewResult Create()
         {
             PackInvitationViewModel packinvitation = new PackInvitationViewModel(User.Identity.GetUserId());
             return PartialView("_InvitePartial", packinvitation);
         }
-
 
         // POST: /Invitation/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -67,37 +66,6 @@ namespace Derby.Controllers
             }
 
             return View(invite);
-        }
-
-        // GET: /Invitation/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PackInvitation packinvitation = db.PackInvitations.Find(id);
-            if (packinvitation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(packinvitation);
-        }
-
-        // POST: /Invitation/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,CreatedDate,ExpiryOffset,Code,InvitedEmail,Accepted,AcceptedUserId")] PackInvitation packinvitation)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(packinvitation).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(packinvitation);
         }
 
         // GET: /Invitation/Delete/5
