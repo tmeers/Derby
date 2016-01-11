aca2dd23692cae506809e57f86e24a14de2bd4db
Tim Meers <tim.meers@gmail.com>
2016-01-10 20:43:45 -0500

    Fix: not saving supplied user name

--- Derby/Controllers/AccountController.cs
+++ Derby/Controllers/AccountController.cs
@@ -366,7 +366,7 @@ namespace Derby
 				{
 					return View("ExternalLoginFailure");
 				}
-				var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+				var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
 				var result = await UserManager.CreateAsync(user);
 				if (result.Succeeded)
 				{
